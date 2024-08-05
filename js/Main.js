e2457d00b67dd91cd2c178c879f5a26823691305
JoshRojasC <josurojascruz@gmail.com>
2024-08-05 16:42:12 -0600

    Feat- Entregale 5

--- js/Main.js
+++ js/Main.js
@@ -5,3 +5,16 @@ document.addEventListener('DOMContentLoaded', ev => {
     MostrarHTML()
 })
 
+
+function MostrarDatosConfiguracion(){
+//truco !fetch
+
+    let url
+fetch('url')
+
+    .then(response => response.json())
+
+    .then(data => console.log(data));
+
+
+}
