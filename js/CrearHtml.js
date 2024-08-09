9deb4c7c4864319e53092f2f061ca68176b2e095
JoshRojasC <josurojascruz@gmail.com>
2024-08-08 18:12:41 -0600

    Feat-Entregable01 Fecha: 06-08-2024

--- js/CrearHtml.js
+++ js/CrearHtml.js
@@ -6,116 +6,52 @@ export function MostrarHTML() {
   let banners = document.getElementById("Banners").innerHTML = CrearBanners();;
   let Tabla = document.getElementById("TablaContainer").innerHTML = CrearTabla();;
 }
-
-function CrearNavbar() {
-  let html = `
-       <a class="navbar-brand" href="#">
-          <img src="img/1-Logo.png" alt="Logo NoticiasTecnológicas" class="navegacion-principal__marca">
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="ListaNavbar">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item"><a class="nav-link" href="#">Inicio</a></li>
-            <li class="nav-item"><a class="nav-link" href="#Noticias">Noticias</a></li>
-            <li class="nav-item"><a class="nav-link" href="#Banner1">Banner1</a></li>
-            <li class="nav-item"><a class="nav-link" href="#Banner2">Banner2</a></li>
-            <li class="nav-item"><a class="nav-link" href="#Footer">Contacto</a></li>
-          </ul>
-          <ul class="navbar-nav">
-            <li class="nav-item"><a class="nav-link" href="#"><i class="fas fa-search"></i></a></li>
-          </ul>
-        </div>
+ /* 1-Navbar */
+ const CrearNavbar = (data) => {
+  let html = 
       `
-
-  return html
-}
-
-
-function CrearFooter() {
-  let html = `
-      <h3>Derechos de Autor de NoticiasTecnológicas</h3>
-        <p>Sigue a NoticiasTecnológicas</p>
-        <ul class="list-inline enlaces-pie" id="ListaFooter">
-          <!-- Aquí se insertarán los elementos del pie de página <li>términos condiciones</li> -->
-          <a href="https://facebook.com" class="mx-2"><i class="fab fa-facebook-f"></i></a>
-          <a href="https://twitter.com" class="mx-2"><i class="fab fa-twitter"></i></a>
-          <a href="https://linkedin.com" class="mx-2"><i class="fab fa-linkedin"></i></a>
+     <a class="navbar-brand" href="#">
+        <img src="img/1-Logo.png" alt="Logo NoticiasTecnológicas" class="navegacion-principal__marca">
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="ListaNavbar">
+        <ul class="navbar-nav mr-auto">
+        `
+        data.forEach(element => {
+          html +=  ` <li class="${element.classDeCss}"><a class="nav-link" href="${element.enlace}">${element.nombre}</a></li> `
+        });
+        
+       
+       html += 
+       `
         </ul>
-      `
+        <ul class="navbar-nav">
+          <li class="nav-item"><a class="nav-link" href="#"><i class="fas fa-search"></i></a></li>        
+          </ul>
+      </div>
+      `   
+    
 
   return html
 }
 
-function CrearTarjetaNoticias() {
+/* 2-Footer */
+const CrearFooter = (data) => {
   let html = `
-       <section class=" banner  col-lg-4 ">
-            <div class="card h-100">
-              <img src="img/1-Noticia.jpg" class="card-img-top" alt="...">
-              <div class="card-body">
-                <h5 class="card-title">Lorem ipsum dolor.</h5>
-                <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                <a href="#" class="btn btn-primary">Más información <i class="fas fa-angle-double-right"></i></a>
-              </div>
-            </div>
-      </section>
-      `
+    <h3>Derechos de Autor de NoticiasTecnológicas</h3>
+      <p>Sigue a NoticiasTecnológicas</p>
+      <ul class="list-inline enlaces-pie" id="ListaFooter">
+        <!-- Aquí se insertarán los elementos del pie de página <li>términos condiciones</li> -->
+        `
+        data.forEach(element => {
+          html += `<a href="${element.enlace}" class="mx-2"><i class="${element.claseDeCss}">${element.nombre}</i></a>` 
+        });
+      
+      html += ` </ul>`
+    
 
   return html
-}
-
-
-function CrearBanners() {
-  let html = `<!-- Banner 1 -->
-      <div class="spaced-div">
-        <section class="banner banner-tarjetas-uno" id="Banner1">
-          <div class="container">
-            <h2>Lorem ipsum dolor.</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut qui laudantium id quam magni accusantium,
-              veritatis, ipsam labore, reprehenderit dolore repudiandae nemo sint deserunt! Suscipit facilis qui
-              inventore
-              consequatur fugit.</p>
-            <a href="#" class="boton">Más información <i class="fas fa-chevron-right"></i></a>
-          </div>
-        </section>
-      </div>
-
-      <!-- Banner 2 -->
-      <div class="spaced-div">
-        <section class="banner banner-tarjetas-dos" id="Banner2">
-          <div class="container">
-            <h2>Lorem ipsum dolor.</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut qui laudantium id quam magni accusantium,
-              veritatis, ipsam labore, reprehenderit dolore repudiandae nemo sint deserunt! Suscipit facilis qui
-              inventore
-              consequatur fugit.</p>
-            <a href="#" class="boton">Más información <i class="fas fa-chevron-right"></i></a>
-          </div>
-        </section>
-      </div>
-  `;
-return html
-}
-
-function CrearTabla(){
-  let html = `
-  <table class="table table-striped table-inverse table-responsive container col-6 text-white" id="Tabla">
-    <thead class="thead-inverse">
-      <tr>
-        <th>Id</th>
-        <th>Nombre</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>1</td>
-        <td>asas</td>
-        <td>asas</td>
-      </tr>
-    </tbody>
-  </table>
-`;
-return html;
 }
